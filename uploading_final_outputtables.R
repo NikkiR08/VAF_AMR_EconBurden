3143290432889c38135f545f7987012de6c13693
Nichola Naylor <nichola.naylor@phe.gov.uk>
2023-06-04 12:26:43 +0100

    code to commit and push end data files (in xls format)

---
+++ uploading_final_outputtables.R
@@ -0,0 +1,24 @@
+#### writing final data tables to a format that will be uploaded 
+
+library(openxlsx)
+
+path1 <- "C:/Users/nichola.naylor/Documents/WHO_2023/VAF_AMR_EconBurden/outputs/"
+
+myfiles = list.files(path=path1,
+                     pattern="*.csv", full.names = TRUE)
+
+
+files_to_upload <- grep("END", myfiles, value = TRUE) 
+
+i <- 1
+
+x <- read.csv(files_to_upload[i])
+
+### extracting the filename from the original pathname
+Split <- strsplit(files_to_upload[i], "/")
+filename <- Split[[1]][length(Split[[1]])]
+filename <- substring(filename,1, nchar(filename)-4)
+
+filename <- paste0(path1, filename,".xls")
+
+write.xlsx(x, filename)
